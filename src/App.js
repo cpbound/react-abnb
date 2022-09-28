19880b4c208c6efe893b621fa633d6b9f661faac
Christian Bound <automaticbound@gmail.com>
2022-09-28 17:37:52 +0900

    map API needs to be moved to MapQuest. Try to update everything to hooks too.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import './App.scss';
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import flats from './data/flats';
+import FlatList from './components/flatList';
+import Marker from './components/marker';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFlat: flats[0],
+      flats
+    };
+  }
+
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
+
+  selectFlat = (index) => {
+    this.setState({ selectedFlat: flats[index] });
+  }
+
+  render() {
+    return (
+      <div>
+        <FlatList
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}
+        />
+        <div className="map-container">
+          <GoogleMapReact defaultCenter={this.center()} defaultZoom={12}>
+            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
+          </GoogleMapReact>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
